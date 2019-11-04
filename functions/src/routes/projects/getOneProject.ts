70fa742bf4612c97443850d05f1afb815a9b4e4a
Scott Benton <scott.ma.benton@gmail.com>
2019-11-03 23:17:20 -0500

    Lets try again, logging the error here

--- functions/src/routes/projects/getOneProject.ts
+++ functions/src/routes/projects/getOneProject.ts
@@ -5,15 +5,14 @@ module.exports = async (req: any, res: any) => {
   const id = req.params.projectId;
   try {
     const projectsSnapshot = db.collection('projects').doc(id).get();
-    const movementRef = db.collection('projects').doc(id).collection('movements').orderBy('index');
-    const movementSnapshot = movementRef.get();
+    const movementSnapshot = db.collection('projects').doc(id).collection('movements').orderBy('index').get();
 
     await projectsSnapshot;
+    console.log(await projectsSnapshot);
     if (!projectsSnapshot) {
       throw new Error('Project ' + id + ' not found.')
     }
     let project: any = {};
-    console.log(projectsSnapshot);
     project = projectsSnapshot.data();
 
 
